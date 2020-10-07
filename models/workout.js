ddce59b5b48a1ef1686076bbf7a8fb599f9847c3
Erika <erika@beasleys.com>
2020-10-07 13:37:32 -0400

    fixed apiRoutes

--- models/workout.js
+++ models/workout.js
@@ -1,38 +1,50 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise type"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise name"
+                },
+                duration: {
+                    type: Number,
+                    required: "Enter an exercise duration in minutes"
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
     },
-    exercises: [{
-        type: {
-            type: String,
-            trim: true,
-            required: 'exercise field cannot be blank.'
-        },
-        name: {
-            type: String,
-            required: 'enter a name for exercise.'
-        },
-        duration: {
-            type: Number
-        },
-        weight: {
-            type: Number
-        },
-        reps: {
-            type: Number
-        },
-        sets: {
-            type: Number
-        },
-        distance: {
-            type: Number
+    {
+        toJSON: {
+            // include any virtual properties when data is requested
+            virtuals: true
         }
-    }]
-});
+    }
+);
 
 // adds a dynamically-created property to schema
 workoutSchema.virtual("totalDuration").get(function () {
@@ -41,7 +53,6 @@ workoutSchema.virtual("totalDuration").get(function () {
         return total + exercise.duration;
     }, 0);
 });
-
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
